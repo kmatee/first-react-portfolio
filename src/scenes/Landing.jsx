f39fccd2e3da836b35a1303c7f370f062a441b7d
kantm <kantm@DESKTOP-H26FR5O>
2023-02-22 20:33:13 +0100

    Add Contact Me and Let's Talk anchors to intro

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -48,7 +48,7 @@ const Landing = ({setSelectedPage}) => {
                         visible: { opacity: 1, x: 0} 
                     }}
                 >
-                    <p className="text-6xl font-playfair z-10 text-center md:text-start">
+                    <p className="text-6xl font-playFair z-10 text-center md:text-start">
                         Máté {""}
                         <span className="xs:relative xs:text-deep-blue xs:font-semibold z-20
                             xs:before:content-brush before:absolute before:-left-[25px]
@@ -60,6 +60,41 @@ const Landing = ({setSelectedPage}) => {
                         Some text about you. Couple sentences long. Nothing crazy, just some
                         basic info about you. Have fun building your own template!
                     </p>
+
+                </motion.div>
+                
+                {/* CALL TO ACTIONS */}
+                <motion.div
+                className="flex mt-6 justify-center md:justify-start"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0} 
+                    }}
+                >
+                    <AnchorLink
+                        className="bg-gradient-rainblue text-deep-blue rounded-sm py-3 px-7 font-semibold
+                            hover:bg-blue hover:text-white transition duration-500"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        Contact Me
+                    </AnchorLink>
+                    <AnchorLink
+                        className="rounded-r-sm bg-gradient-rainblue py-0.5 pr-0.5"
+                        onClick={() => setSelectedPage("contact")}
+                        href="#contact"
+                    >
+                        <div
+                            className="bg-deep-blue hover:text-red transition duration-500 w-full h-full flex intems-center
+                                justify-center font-playFair px-10 py-2"
+                        >
+                            Let's talk.
+                        </div>
+                    </AnchorLink>
 
                 </motion.div>
 
