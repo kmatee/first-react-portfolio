71cb010da0cc68e23f6b792e070849783d9bf06a
kantm <kantm@DESKTOP-H26FR5O>
2023-02-23 15:42:20 +0100

    Change footer text slightly

--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <SocialMediaIcons/>
             <div className="md:flex justify-center md:justify-between text-center">
                 <p className="font-playFair font-semibold text-2xl text-yellow">MÁTÉ KÁNTOR</p>
-                <p className="font-playFair text-md text-yellow">©2023 KÁNTOR. All Rigths Reserved</p>
+                <p className="font-playFair text-md text-yellow">©2023 KÁNTOR. All Rigths Reserved.</p>
             </div>
 
         </div>
