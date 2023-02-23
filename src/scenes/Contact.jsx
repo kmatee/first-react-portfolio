8baa6894f1310edbcfd572509bf2a84ee495b3f7
kantm <kantm@DESKTOP-H26FR5O>
2023-02-23 14:53:15 +0100

    Add contact form with validation and send email with from data

---
+++ src/scenes/Contact.jsx
@@ -0,0 +1,133 @@
+import LineGradient from "../components/LineGradient"
+import {useForm} from "react-hook-form"
+import { motion } from "framer-motion"
+
+const Contact = () => {
+
+    const {
+        register,
+        trigger,
+        formState: {errors}
+    } = useForm();
+
+    const onSubmit = async (e) => {
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+    }
+
+    return (
+        <section id="contact" className="py-48">
+            {/* HEADINGS */}
+            <motion.div
+                className="flex justify-end w-full"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 0.5 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0} 
+                }}
+            >
+                <div>
+                    <p className="font-playFair font-semibold text-4xl">
+                        <span className="text-yellow">CONTACT ME </span>TO GET STARTED
+                    </p>
+                    <div className="flex md:justify-end my-5">
+                        <LineGradient width="w-2/5" />
+                    </div>
+                </div>               
+            </motion.div>      
+            {/* FORM AND IMAGE */}
+            <div className="md:flex md:justify-between gap-1 mt-5">
+                <motion.div
+                    className="basis-1/2 flex justify-center"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0} 
+                    }}
+                >
+                    <img src="../assets/contact-image.jpeg" alt="contact" />
+                </motion.div>
+                <motion.div
+                    className="basis-1/2 mt-10 md:mt-0"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, y: 50 },
+                        visible: { opacity: 1, y: 0} 
+                    }}
+                >
+                    <form
+                        action="https://formsubmit.co/5bc98c915281722218ae871f7df11406"
+                        target="_blank"
+                        onSubmit={onSubmit}
+                        method="POST"
+                    >
+                        <input
+                            className="w-full bg-blue font-semibold placeholder-opaque-black p-3"
+                            type="text"
+                            placeholder="NAME"
+                            {...register("name",{
+                                required: true,
+                                maxLength: 100
+                            })}    
+                        />
+                        {errors.name && (
+                            <p className="text-red mt-1">
+                                {errors.name.type === "required" && "This field is required!"}
+                                {errors.name.type === "maxLength" && "Your name can't be longer than 100 characters"}
+                            </p>
+                        )}
+                        <input
+                            className="w-full bg-blue font-semibold placeholder-opaque-black p-3 mt-5"
+                            type="text"
+                            placeholder="EMAIL"
+                            {...register("email",{
+                                required: true,
+                                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+ \.[A-Z]{2,}$/i
+                            })}    
+                        />
+                        {errors.email && (
+                            <p className="text-red mt-1">
+                                {errors.email.type === "required" && "This field is required!"}
+                                {errors.email.type === "pattern" && "Invalid email address."}
+                            </p>
+                        )}
+                        <textarea
+                            className="w-full bg-blue font-semibold placeholder-opaque-black p-3 mt-5"
+                            type="text"
+                            placeholder="MESSAGE"
+                            rows="4"
+                            cols="50"
+                            {...register("message",{
+                                required: true,
+                                maxLength: 1500
+                            })}    
+                        />
+                        {errors.message && (
+                            <p className="text-red mt-1">
+                                {errors.message.type === "required" && "This field is required!"}
+                                {errors.message.type === "maxLength" && "The message can'tbe longer than 1500 characters"}
+                            </p>
+                        )}
+                        <button className="p5 bg-yellow font-semibold text-deep-blue mt-5 hover:bg-red
+                            hover:text-white transition duration-500" type="submit">
+                            SEND ME A MESSAGE
+                        </button>
+                    </form>
+                </motion.div>
+            </div>      
+        </section>
+    )
+}
+
+export default Contact
